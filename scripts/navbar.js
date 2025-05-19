7ae1d90a62f2b5495e9e079a97266b20daa41281
Ellen Ristow <ellen.ristow@gmail.com>
2025-05-19 10:41:58 +0100

    add notes

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-	fetch('../navbar.html')
-		.then(res => res.text())
+	//DomCL - é um evento disparado após o html ser carregado
+	fetch('../navbar.html') //busca o ficheiro navbar.html e retorna uma promisse com uma resposta
+		.then(res => res.text()) //assim que recebe converte o conteúdo em texto (aqui html)
 		.then(data => {
 			document.getElementById('navbar-placeholder').innerHTML = data;
-		});
+		}); //assim que for convertido em texto, será injetado num elemento com id 'navbar-placeholder' pelo innerHTML
 });
