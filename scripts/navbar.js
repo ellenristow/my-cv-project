8ea01c0e133f2ac2e0657ef1a4fd27fa460ab74b
ellenristow <ellenristow@gmail.com>
2025-11-16 17:01:08 +0000

    created nav bar and added styles

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,6 +1,6 @@
-const navBar = document.querySelector('nav');
+const navBar = document.querySelector('.nav');
 window.addEventListener('scroll', function () {
-	if (window.scrollY > 0) {
+	if (window.scrollY > 100) {
 		navBar.classList.add('navbar-background');
 	} else {
 		navBar.classList.remove('navbar-background');
